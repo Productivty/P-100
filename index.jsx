1d8348b7cbe4d72cf120163c924cc6b04d37404e
Pradeep_thapa <119683727+Productivty@users.noreply.github.com>
2023-11-12 01:22:22 +0530

    3rd commit

--- index.jsx
+++ index.jsx
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { RouterProvider } from "react-router-dom/dist";
-import { appRouter } from "./src/App"
-
-
+import { appRouter } from "./src/App";
--- src/pages/ErrorComponent.jsx
+++ src/pages/ErrorComponent.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { useRouteError } from 'react-router-dom/dist'
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const ErrorComponent = () => {
-    const err = useRouteError()
-    // console.log(err)
-    return (
-        < div className='error-message' >
-            <h1>{err.status} : {err.statusText}</h1>
-            <h2>Oops! Try It Again</h2>
-            <h3>{err.error.message}</h3>
-        </div >
-    )
-}
+  const err = useRouteError();
+  return (
+    <div className="error-message">
+      <h1>
+        {err.status} : {err.statusText}
+      </h1>
+      <h2>Oops! Try It Again</h2>
+      <h3>{err.error.message}</h3>
+    </div>
+  );
+};
 
-export default ErrorComponent
+export default ErrorComponent;
